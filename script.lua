fd81cac646bbc867183cdd941485220d3639f66d
SDCLxD <123998607+SDCLxD@users.noreply.github.com>
2023-12-15 12:12:18 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -18,33 +18,70 @@ Tab1:AddToggle({
 	Callback = function(Value)
         getgenv().farm = Value
 
-        function atirar()
-            while task.wait(.4) do
-                local head = game.Workspace.NPCs["Fishman Karate User"].HumanoidRootPart.Position
-                local args = {
-                    [1] = "reload",
-                    [2] = {
-                        ["Gun"] = "Rifle"
+        function quest()
+            local data = game.ReplicatedStorage["Stats"..game.Players.LocalPlayer.Name]
+            if data.Stats.Level.Value > 189 and data.Quest.CurrentQuest.Value == "None" then
+                local speed = 10000
+                local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+                local CFrameEnd = CFrame.new(7731.00537, -2175.83203, -17222.9922)
+                local info = TweenInfo.new((game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position).magnitude / speed,Enum.EasingStyle.Linear)
+                local tween = game:GetService("TweenService"):Create(pl, info, {CFrame = CFrameEnd})
+                tween:Play()
+                tween.Completed:Wait()
+                if tween.Completed then
+                    local args = {
+                        [1] = {
+                            [1] = "takequest",
+                            [2] = "Help becky"
+                        }
                     }
-                }
-                
-                game:GetService("ReplicatedStorage").Events.CIcklcon.gunFunctions:InvokeServer(unpack(args))
-                local args = {
-                    [1] = "fire",
-                    [2] = {
-                        ["Start"] = CFrame.new(7826.61865, -2129.48486, -17134.5547),
-                        ["Gun"] = "Rifle",
-                        ["joe"] = "true",
-                        ["Position"] = Vector3.new(head.X, head.Y, head.Z)
-                    }
-                }
-                
-                game:GetService("ReplicatedStorage").Events.CIcklcon:FireServer(unpack(args))
+                                                
+                    game:GetService("ReplicatedStorage").Events.Quest:InvokeServer(unpack(args))
+                end
             end
         end
 
-        while getgenv().farm and task.wait() do
-            atirar()
+            function shoot()
+                while true and task.wait(0.4) do
+                    local head = game.Workspace.NPCs["Fishman Karate User"].Head.Position
+                    local recarregar = {
+                        [1] = "reload",
+                        [2] = {
+                            ["Gun"] = "Rifle"
+                        }
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.CIcklcon.gunFunctions:InvokeServer(unpack(recarregar))
+
+                    local atirar = {
+                        [1] = "fire",
+                        [2] = {
+                            ["Start"] = CFrame.new(7837.54395, -2151.33203, -17133.9707),
+                            ["Gun"] = "Rifle",
+                            ["joe"] = "true",
+                            ["Position"] = Vector3.new(head.X, head.Y, head.Z)
+                        }
+                    }
+
+                    game:GetService("ReplicatedStorage").Events.CIcklcon:FireServer(unpack(atirar))
+                end
+            end
+
+            function position()
+                local vt = 5000
+                local pl = game.Players.LocalPlayer.Character.HumanoidRootPart
+                local CFrameEnd = CFrame.new(7837.54395, -2151.33203, -17133.9707)
+                local info = TweenInfo.new((game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").Position).magnitude / vt,Enum.EasingStyle.Linear)
+                local tween = game:GetService("TweenService"):Create(pl, info, {CFrame = CFrameEnd})
+                tween:Play()
+                tween.Completed:Wait()
+            end
+                    
+            while getgenv().farm and task.wait() do
+                quest()
+                position()
+                shoot()
+            end
         end
 	end    
 })
@@ -57,52 +94,3 @@ Tab1:AddButton({
         pl.CFrame = location
   	end    
 })
-
-local Tab2 = Window:MakeTab({
-	Name = "Tab 2 : Stats",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-local Section = Tab2:AddSection({
-	Name = "Section 2 : Auto Stats"
-})
-
-local data = game.ReplicatedStorage["Stats"..game.Players.LocalPlayer.Name]
-
-getgenv().Setting = {
-    AutoStat = {
-        Strength = false,
-        Stamina = false,
-        Defense = false,
-        GunMastery = false,
-        SwordMastery = false
-    }
-}
-
-for k, v in pairs(Setting.AutoStat) do
-    Tab2:AddToggle({
-        Name = k,
-        Default = false,
-        Callback = function(Value)
-            Setting.AutoStat[k] = Value
-        end    
-    })
-end
-
-function autostat()
-    spawn(
-        function()
-            while wait(.5) do
-                if Setting.AutoStat then
-                    for k, v in pairs(Setting.AutoStat) do
-                        if v and data.Stats.SkillPoints.Value > 0 then
-                        game:GetService("ReplicatedStorage").Events.stats:FireServer(k, nil, 1)
-                    end
-                end
-            end
-        end
-    end)
-end
-
-autostat()
